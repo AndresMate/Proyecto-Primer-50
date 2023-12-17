35a381a75ba465dc92651651728351ee51724067
AndresMate <andres.mateus01@uptc.edu.co>
2023-12-17 09:35:59 -0500

    mas eror

--- Code/src/Test/Main.java
+++ Code/src/Test/Main.java
@@ -6,11 +6,14 @@ public class Main {
         Dictionary dictionary = new Dictionary();
         System.out.println(dictionary.addWordToDictionary("hola"));
         System.out.println(dictionary.addWordToDictionary("holo"));
+<<<<<<< HEAD
         System.out.println(dictionary.addWordToDictionary("hala"));
         System.out.println(dictionary.addWordToDictionary("dumar"));
         System.out.println(dictionary.addWordToDictionary("dinamarca"));
         System.out.println(dictionary.addWordToDictionary("diana"));
         System.out.println(dictionary.convertWordToASCII("dumar"));
+=======
+>>>>>>> parent of cec18c0 (mas test)
         dictionary.displayDictionary();
 
 
